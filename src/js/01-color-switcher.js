323921d3284f08d06f263a31b848a474869ad393
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-25 23:22:02 +0200

    done task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let timerColor;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.start.addEventListener('click', onBtnStartClick);
+refs.stop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  timerColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.setAttribute('disabled', 'disabled');
+}
+
+function onBtnStopClick() {
+  refs.start.removeAttribute('disabled', 'disabled');
+  clearInterval(timerColor);
+}
