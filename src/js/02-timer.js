0214afa1d5a879c03d5d04ef6e1c902a3576655a
Anatolii Kobzar <TohaPirat@gmail.com>
2022-12-28 15:51:41 +0200

    update task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -33,6 +33,31 @@ flatpickr('#datetime-picker', options);
 
 startBtn.addEventListener('click', onBtnStartClick);
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(200000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(44324140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
 function onBtnStartClick() {
-  console.log(selectedDate[0]);
+  const date = new Date();
+  const dataTimer = selectedDate[0] - date;
+  console.log(convertMs(dataTimer));
 }
